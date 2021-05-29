a645b2f64fd53f0a8e5e235f2944ada244eca7be
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-05-29 20:09:22 +0900

    error

--- includes/error.h
+++ includes/error.h
@@ -1,9 +1,18 @@
 #ifndef ERROR_H
 # define ERROR_H
 
-#define ERR_SYNTAX 0
+#define ERR_QUOTE 0
+#define ERR_SEMICOLONE 1
+#define ERR_SEMICOLONE2 2
+#define ERR_PIPE 3
+#define ERR_PIPE2 4
 
 #include <stdio.h>
 
+/*
+** error.c
+*/
+
+int		print_syntax_error(int errno);
 
 #endif
--- srcs/error/error.c
+++ srcs/error/error.c
@@ -1,7 +1,22 @@
 #include "../../includes/error.h"
 
-int		print_error(int errno)
+/*
+** syntax error print
+** return 0:failed 1:succeed 
+*/
+int		print_syntax_error(int errno)
 {
-	if (errno == ERR_SYNTAX)
-		write(2, "bash: syntax error near unexpected token `;", 43);
+	if (errno == ERR_QUOTE)
+		write(2, "bash: syntax error quote not closed", 43);
+	if (errno == ERR_SEMICOLONE)
+		write(2, "bash: syntax error near unexpected token `;'", 45);
+	if (errno == ERR_SEMICOLONE2)
+		write(2, "bash: syntax error near unexpected token `;;'", 46);
+	if (errno == ERR_PIPE)
+		write(2, "bash: syntax error near unexpected token `|'", 45);
+	if (errno == ERR_PIPE2)
+		write(2, "bash: syntax error near unexpected token `||'", 46);
+	else
+		return (0);
+	return (1);
 }
