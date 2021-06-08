76e7e8517b71118168dd8a22e940e3db9c763fe6
Youngseon Ahn <yoahn@c4r1s8.42seoul.kr>
2021-06-08 20:16:37 +0900

    [edit] export add

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -4,6 +4,13 @@ extern t_state	g_state;
 
 void	ft_export(t_command *cmd)
 {
-	(void)cmd;
+	int		i;
+
+	i = 0;
 	g_state.ret = 0;
+	while (cmd->args[i])
+	{
+		env_add(&g_state.env, env_new(cmd->args[i]));
+		i++;
+	}
 }
